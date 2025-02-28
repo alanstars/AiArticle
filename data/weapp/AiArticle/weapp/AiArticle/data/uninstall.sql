aac0ee90414520524d59fb96c75480da2feaf9f7
alan's MacBook Pro <251956250@qq.com>
2025-02-28 16:41:40 +0800

    update

--- data/weapp/AiArticle/weapp/AiArticle/data/uninstall.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS `#@__weapp_ai_article`;
-DROP TABLE IF EXISTS `#@__weapp_ai_article_conf`;
-DROP TABLE IF EXISTS `#@__weapp_ai_article_lists`;
--- data/weapp/AiArticle/weapp/AiArticle/lang/zh-cn.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'plugin_name' => 'Ai文章自动更新',
-    'description' => 'Ai 自动生成文章，定时自动更新，支持多语言',
-    // 其他需要翻译的文本
-];
