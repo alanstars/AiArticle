c156d49b35dde7a475e7e130a0fd461ca6dc3a22
alan's MacBook Pro <251956250@qq.com>
2025-03-01 17:37:28 +0800

    sql

--- weapp/AiArticle/data/install.sql
+++ weapp/AiArticle/data/install.sql
@@ -14,14 +14,14 @@ CREATE TABLE `#@__weapp_ai_article` (
   `ai_model_name` varchar(100) NOT NULL COMMENT 'AI模型名称',
   `ai_model_identifier` varchar(100) NOT NULL COMMENT 'AI模型标识',
   `article_theme` varchar(255) DEFAULT NULL COMMENT '生成文章主题',
-  `status` tinyint(1) DEFAULT '0' COMMENT '生成状态：1=>已完成；0=>未完成',
+  `status` tinyint(1) unsigned zerofill NOT NULL DEFAULT '0' COMMENT '生成状态：1=>已完成；0=>未完成',
   `article_language` varchar(50) DEFAULT 'zh' COMMENT '文章语言',
   `article_count` int(11) DEFAULT '1' COMMENT '生成文章数量',
   `bind_column_id` int(11) DEFAULT NULL COMMENT '要绑定的栏目ID',
   `bind_column_id_lang` varchar(20) DEFAULT NULL COMMENT '绑定的中文栏目ID,逗号或者序列化格式:ID-语言标识',
   `is_translate` tinyint(1) DEFAULT '0' COMMENT '是否翻译并发布到其他语言',
   `publish_schedule` text COMMENT '定时发布规则（序列化格式）',
-  `publish_count` int(11) DEFAULT '1' COMMENT '每次发布的文章数量，默认为 1',
+  `publish_count` int(11) DEFAULT '1' COMMENT '每次发布的文章数量，默认为1',
   `created_time` int(11) NOT NULL COMMENT '创建时间',
   `updated_time` int(11) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
@@ -37,9 +37,9 @@ CREATE TABLE `#@__weapp_ai_article_conf` (
   `ai_model_name` varchar(100) NOT NULL COMMENT 'AI模型名称',
   `ai_model_identifier` varchar(100) NOT NULL COMMENT 'AI模型标识',
   `ai_config_key` varchar(255) NOT NULL COMMENT 'AI配置Key',
-  `translate_model_name` varchar(100) DEFAULT NULL COMMENT '翻译模型名称',
-  `translate_model_identifier` varchar(100) DEFAULT NULL COMMENT '翻译模型标识',
-  `translate_config_key` varchar(255) DEFAULT NULL COMMENT '翻译配置Key',
+  `translate_model_name` varchar(100) DEFAULT '' COMMENT '翻译模型名称',
+  `translate_model_identifier` varchar(100) DEFAULT '' COMMENT '翻译模型标识',
+  `translate_config_key` varchar(255) DEFAULT '' COMMENT '翻译配置Key',
   `created_time` int(11) NOT NULL COMMENT '创建时间',
   `updated_time` int(11) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
@@ -48,7 +48,7 @@ CREATE TABLE `#@__weapp_ai_article_conf` (
 -- ----------------------------
 -- Records of #@__weapp_ai_article_lists
 -- 生成文章的记录表
--- ----------------------------------
+-- ----------------------------
 DROP TABLE IF EXISTS `#@__weapp_ai_article_lists`;
 CREATE TABLE `#@__weapp_ai_article_lists` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
@@ -57,7 +57,7 @@ CREATE TABLE `#@__weapp_ai_article_lists` (
   `typeid` int(11) DEFAULT NULL COMMENT '要发布的栏目 ID',
   `aid` int(11) DEFAULT NULL COMMENT '文章ID，如果已发布则更新该 ID',
   `title` varchar(255) NOT NULL COMMENT '发布的文章标题',
-  `content` text NOT NULL COMMENT '发布的文章内容',
+  `content` text COMMENT '发布的文章内容',
   `seo_title` varchar(255) DEFAULT NULL COMMENT '发布的SEO标题',
   `seo_keywords` varchar(255) DEFAULT NULL COMMENT '发布的SEO关键词',
   `seo_description` text COMMENT '发布的SEO描述',
@@ -71,3 +71,4 @@ CREATE TABLE `#@__weapp_ai_article_lists` (
   `finish_reason` varchar(100) DEFAULT '' COMMENT '结束原因：stop 正常',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='AI文章列表';
+
